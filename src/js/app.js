df8a7c3c5405433d6534eb7368bb2fcfdb2edeee
Michael Reynolds <michael4reynolds@gmail.com>
2017-04-18 17:11:37 -0400

    game play no AI yet

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,36 @@
 import log, {hookConsoleLog} from 'stacklogger'
-import classNames from 'classnames'
-import moment from 'moment'
-import 'moment-duration-format'
-import Overtimer from 'overtimer'
 
 // Model
+const player = (name, mark = 'O') => ({name, mark})
+const player1 = player('Hal', 'X')
+const player2 = player('Me', 'O')
+let currentPlayer = player1
+
+const board = [
+  '', '', '',
+  '', '', '',
+  '', '', ''
+]
 
 // View
+const squares = Array.from(document.querySelectorAll('.game span'))
 
 // Controller
+const changeTurn = () => {
+  currentPlayer = currentPlayer === player1 ? player2 : player1
+}
 
-// Events
+const placeMark = e => {
+  e.target.innerText = currentPlayer.mark
+  board[e.target.id.split('-')[1]] = currentPlayer.mark
+  changeTurn()
+}
 
 // initialize
 function init() {
   try {
-    console.log('play tic-tac-toe')
+    // Events
+    squares.forEach(el => el.onclick = placeMark)
   } catch (e) {
     log(e)
   }
