7fcd210a2f9d38da82cb4c5c58a389a4ace80ae1
Michael Reynolds <michael4reynolds@gmail.com>
2017-04-21 20:43:21 -0400

    add legal moves and game state flags

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,9 @@ const player1 = player('Hal', 'X')
 const player2 = player('Me', 'O')
 let currentPlayer = player1
 let moveCount = 0
-
+let waiting = false
+let gameOver = false
+const combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
 const board = [
   '', '', '',
   '', '', '',
@@ -33,33 +35,69 @@ const play = square => {
   moveCount++
 }
 
-const getRandomIntInclusive = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-const delay = amount => new Promise(resolve => setTimeout(resolve, amount))
+const delay = amount => new Promise(resolve => setTimeout(() => {
+  waiting = false
+  resolve()
+}, amount))
+
+const checkForWin = () => combos.some(c =>
+  c.every(mark => board[mark] === currentPlayer.mark))
+
+const checkForDraw = () => moveCount === 9
+
+const legalMoves = () => board.reduce((a, e, i) => {
+  if (e === '') a.push(i)
+  return a
+}, [])
+
+const moveLoop = async e => {
+  if (e.target.innerText || waiting || gameOver) return
+  placeMark(e)
+  if (checkForWin()) {
+    gameOver = true
+    log(currentPlayer.name, 'wins!!!')
+    return
+  }
+  if (checkForDraw()) {
+    log('Draw!')
+    return
+  }
+  changeTurn()
+  waiting = true
+  await delay(1200)
+  // todo: playWinningMove()
+  // todo: blockOpponent()
+  let legal = legalMoves()
+  log(legal)
+  play(legal[getRandomInt(0, legal.length)])
+  if (checkForWin()) {
+    gameOver = true
+    log(currentPlayer.name, 'wins!!!')
+    return
+  }
+  if (checkForDraw()) {
+    log('Draw!')
+    return
+  }
+  changeTurn()
+}
 
 // Events
-squares.forEach(el => el.onclick = async e => {
-  placeMark(e)
-  // todo: checkForWin()
-  changeTurn()
-  await delay(1200)
-  // todo: checkForBlock()
-  // todo: checkForWinningMove()
-  play(getRandomIntInclusive(0, 8))
-  // todo: checkForWin()
-  changeTurn()
-})
+squares.forEach(el => el.onclick = moveLoop)
 
 // initialize
 function init() {
   try {
-    play(getRandomIntInclusive(0, 8))
+    let legal = legalMoves()
+    log(legal)
+    play(legal[getRandomInt(0, legal.length)])
     changeTurn()
-    log(moveCount)
   } catch (e) {
     log(e)
   }
